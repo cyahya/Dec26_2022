10963d0f50525168b48c5ab1a85583b8b1f74d8f
cyahya <46780636+cyahya@users.noreply.github.com>
2022-12-28 15:15:11 -0500

    Add files via upload

---
+++ Module 7/Dog.java
@@ -0,0 +1,24 @@
+package Animals;
+
+public class Dog extends animal{
+
+	//OOC implementation
+	// child class extends parent class
+	
+	//Constructor
+	public Dog(){
+		System.out.println("It's Dog constructor");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+
+	
+    public static void bark(){
+    	System.out.println("ghaow ghaow");
+	}
+}
