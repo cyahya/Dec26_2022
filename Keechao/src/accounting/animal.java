9fa917089456ffee46e1a418972fafc6ddcf630e
cyahya <46780636+cyahya@users.noreply.github.com>
2022-12-28 14:25:40 -0500

    Add files via upload

---
+++ Keechao/src/accounting/animal.java
@@ -0,0 +1,37 @@
+package Animals;
+
+public class animal {
+
+	public static int x = 5;
+	
+	//Constructor
+	public animal(){
+		System.out.println("It's animal constructor");
+	}
+	
+	
+	//method
+	public static void FourLegs(){
+		//code
+		System.out.println("It has four legs");
+	}
+		
+    public static void TwoEyes(){
+    	//code
+    	System.out.println("It has two eyes");
+	}
+	
+    public static void OneTail(){
+    	//code
+    	System.out.println("It has one tail");
+	}
+	
+	  
+	
+	
+	
+	
+	
+	
+	
+}
